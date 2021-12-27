83b76604e97c07993892d39fb0056a4d181fe854
Michelle Otero <michelle.otero@lc.cuny.edu>
2021-12-27 14:23:32 -0500

    Base Commit

---
+++ React/final project/finalwebsite/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Button from '@material-ui/core/Button'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Button 
+        size="large"
+        varient="containted" 
+        color="secondary"> 
+          Hello World
+        </Button>
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
